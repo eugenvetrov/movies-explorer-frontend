9616bf5a52dacc96f8410161467283637dd33c19
Eugene Vetrov <evg.vetrow@yandex.ru>
2022-07-11 15:09:41 +0500

    Make section about project

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,6 +3,7 @@ import { Routes, Route} from "react-router-dom";
 import Header from '../Header/Header';
 import HeaderLanding from '../HeaderLanding/HeaderLanding.js';
 import LandingSelector from '../LandingSelector/LandingSelector';
+import AboutProject from '../AboutProject/AboutProject';
 
 function App() {
 
@@ -16,6 +17,7 @@ function App() {
               <>
               <HeaderLanding />
               <LandingSelector />
+              <AboutProject />
               </>
             }
           />
