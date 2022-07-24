3a23dab23c6db505ac437f6d80af3c624ca02c8e
Eugene Vetrov <evg.vetrow@yandex.ru>
2022-07-25 01:26:46 +0500

    Add list teg to portfolio section

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -6,18 +6,26 @@ const Portfolio = () => {
     return(
         <section className="portfolio">
           <p className="portfolio__title">Портфолио</p>
-          <a className="portfolio__link" href="https://eugenvetrov.github.io/how-to-learn/" target="_blank" rel='noreferrer'>
-            <p className="portfolio__link-text">Статичный сайт</p>
-            <img className="portfolio__link-image" src={arrowLink} alt="Картинка стрелки для ссылки"/>
-          </a>
-          <a className="portfolio__link" href="https://eugenvetrov.github.io/yet-another-project/" target="_blank" rel='noreferrer'>
-            <p className="portfolio__link-text">Адаптивный сайт</p>
-            <img className="portfolio__link-image" src={arrowLink} alt="Картинка стрелки для ссылки"/>
-          </a>
-          <a className="portfolio__link" href="http://evg.vetrow.mesto.nomoreparties.sbs" target="_blank" rel='noreferrer'>
-            <p className="portfolio__link-text">Одностраничное приложение</p>
-            <img className="portfolio__link-image" src={arrowLink} alt="Картинка стрелки для ссылки"/>
-          </a> 
+          <ul className="portfolio__list">
+            <li className="portfolio__point">
+            <a className="portfolio__link" href="https://eugenvetrov.github.io/how-to-learn/" target="_blank" rel='noreferrer'>
+              <p className="portfolio__link-text">Статичный сайт</p>
+              <img className="portfolio__link-image" src={arrowLink} alt="Картинка стрелки для ссылки"/>
+            </a>
+            </li>
+            <li className="portfolio__point">
+            <a className="portfolio__link" href="https://eugenvetrov.github.io/yet-another-project/" target="_blank" rel='noreferrer'>
+              <p className="portfolio__link-text">Адаптивный сайт</p>
+              <img className="portfolio__link-image" src={arrowLink} alt="Картинка стрелки для ссылки"/>
+            </a>
+            </li>
+            <li className="portfolio__point">
+            <a className="portfolio__link" href="http://evg.vetrow.mesto.nomoreparties.sbs" target="_blank" rel='noreferrer'>
+              <p className="portfolio__link-text">Одностраничное приложение</p>
+              <img className="portfolio__link-image" src={arrowLink} alt="Картинка стрелки для ссылки"/>
+            </a>
+            </li>
+          </ul>
         </section>
     )
 }
